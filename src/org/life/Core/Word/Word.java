4f05b9e258644696b5fa938cdd9f955ceaa2e40e
Yang-Pycoder <1197094508@qq.com>
2016-03-05 14:27:02 +0800

    修改说明，补充 Word 策略，可动态对不同的表进行读与写

--- src/org/life/Core/Word/Word.java
+++ src/org/life/Core/Word/Word.java
@@ -14,16 +14,17 @@ import java.util.*;
  *                            value+
  *
  * 策略：
- * 从A字段开始向A字段设定的方位开始循环读取空 TableCell, 直到遇到非空 XWPFTableCell 为止
- * 遇到一个 String 对应多个 XWPFTableCell 时, 对 String 转为 char[] 平均分到各个 TableCell 内
+ * 从A字段开始向A字段设定的方位开始循环读取空 XWPFTableCell, 直到遇到非空 XWPFTableCell 为止
+ * 遇到一个 String 对应多个 XWPFTableCell 时, 对 String 转为 char[] 平均分到各个 XWPFTableCell 内
  *
  * 初始化时立刻对当前 Word 进行遍历, 获取所有的表及其字段, 使用 Map 组成一个 Field-XWPFTableCell 键值对结构
  * 同时对 TableIndex 进行关联, 把对 Word 的操作转换为对 Map 的操作
  * 一个 Word 对象仅处理一份 Word 文档, 由构建器保证
  *
- * dataDirectionMap 字段, 方位
- * tableMap 表索引号, field-TableCell Map(表结构)
- * cellMap 字段, XWPFTableCell对象
+ * tableMap 表格Map，一个 entry 便是一个表格
+ * dataDirectionMap 字段方位，key-字段 value-方位 ，决定字段读取方向
+ * metaDataAndLengthMap 元数据及其数据长度， key-元数据 value-数据长度，非 null 时为读取模式
+ * trimSet 过滤字符集合 将会过滤掉在此列表中所有的字符
  */
 public class Word implements WordProcessor {
     private Map<Integer, Map<String, List<XWPFTableCell>>> tableMap;
@@ -47,10 +48,13 @@ public class Word implements WordProcessor {
         if(null == dataDirectionMap || dataDirectionMap.size() == 0)throw new NullPointerException(String
                 .format("%s: dataDirectionMap is not init (constructor: Word).", getClass().getName()));
 
-        scanning$empty();
+        scanning();
     }
 
-    private void scanning$full()
+    /**
+     * 扫描非空单元格(读取模式)
+     */
+    private void scanning()
     {
         int rowNum = 0;
         int cellNum = 0;
@@ -77,7 +81,8 @@ public class Word implements WordProcessor {
                     continue;
                 }
 
-                if(! metaDataAndLengthMap.containsKey(cell.getText()))
+                // 非读取模式下跳过空字段
+                if(null == metaDataAndLengthMap && cell.getText().equals(""))
                 {
                     cellNum++;
                     continue;
@@ -90,65 +95,19 @@ public class Word implements WordProcessor {
                 // 此时的 rowNum 与 cellNum 均为 key 的方位！！！
                 if(dataDirectionMap.containsKey(key)) direction = dataDirectionMap.get(key);
 
-                scanning$core(table, key, cellMap, direction, rowNum, cellNum);
+                if(metaDataAndLengthMap != null)scanning$reader();
+                else scanning$writer(table, key, cellMap, direction, rowNum, cellNum);
                 cellNum++;
             }
         }
     }
 
-    private void scanning$core()
+    private void scanning$reader()
     {
 
     }
 
-    private void scanning$empty()
-    {
-        int rowNum = 0;
-        int cellNum = 0;
-        int tableCount = 1;
-        List<XWPFTable> tables = doc.getTables();
-
-        for(XWPFTable table: tables)
-        {
-            Map<String, List<XWPFTableCell>> cellMap = new HashMap<>();
-            tableMap.put(tableCount, cellMap);
-            tableCount++;
-
-            final int totalRow = table.getNumberOfRows();
-
-            while(rowNum < totalRow)
-            {
-                XWPFTableCell cell = table.getRow(rowNum).getCell(cellNum);
-
-                //  cell 为 null 代表当前 row 已空
-                if(null == cell)
-                {
-                    rowNum++;
-                    cellNum = 0;
-                    continue;
-                }
-
-                // cell 为空时跳过本次训话
-                if(cell.getText().equals(""))
-                {
-                    cellNum++;
-                    continue;
-                }
-
-                // 到这里开始获取 key 并设定数据方向
-                int direction = -1;
-                String key = trim(cell.getText());
-
-                // 此时的 rowNum 与 cellNum 均为 key 的方位！！！
-                if(dataDirectionMap.containsKey(key)) direction = dataDirectionMap.get(key);
-
-                scanning$core(table, key, cellMap, direction, rowNum, cellNum);
-                cellNum++;
-            }
-        }
-    }
-
-    private void scanning$core(XWPFTable table, String key, Map<String, List<XWPFTableCell>> cellMap,
+    private void scanning$writer(XWPFTable table, String key, Map<String, List<XWPFTableCell>> cellMap,
                                int direction, int rowNumCopy, int cellNumCopy)
     {
         while(true)
@@ -160,7 +119,7 @@ public class Word implements WordProcessor {
             XWPFTableCell cell;
             try {
                 cell = table.getRow(rowNumCopy).getCell(cellNumCopy);
-                if(null == cell || (! cell.getText().equals("")))return;
+                if(null == cell || (! cell.getText().equals("")))return;   //  此处根据策略而变
             }
             catch (NullPointerException e) {
                 return;
